98fb1e3042adddf3e8ac798355f00d9b4cd01176
ytgw <example@example>
2021-02-04 20:49:30 +0000

    イテレータパターンの練習問題のコードを追加

---
+++ 01_iterator/q1/BookShelf.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class BookShelf implements Aggregate {
+    private ArrayList<Book> books;
+    public BookShelf(int initialsize) {
+        this.books = new ArrayList<Book>(initialsize);
+    }
+    public Book getBookAt(int index) {
+        return books.get(index);
+    }
+    public void appendBook(Book book) {
+        books.add(book);
+    }
+    public int getLength() {
+        return books.size();
+    }
+    public Iterator iterator() {
+        return new BookShelfIterator(this);
+    }
+}
