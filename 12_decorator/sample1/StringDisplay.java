1d2f7bbad18dfe115bf84ea38f723609b3da1fd0
ytgw <tagawa0920@gmail.com>
2021-07-10 07:11:37 +0000

    全角半角文字列両者に対応できるgetColumnsメソッドに変更

--- 12_decorator/sample1/StringDisplay.java
+++ 12_decorator/sample1/StringDisplay.java
@@ -4,7 +4,18 @@ public class StringDisplay extends Display {
         this.string = string;
     }
     public int getColumns() {
-        return string.getBytes().length;
+        char[] chars = string.toCharArray();
+        int columns = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (String.valueOf(chars[i]).getBytes().length < 2) {
+                // 半角の場合
+                columns += 1;
+            } else {
+                // 全角の場合
+                columns += 2;
+            }
+        }
+        return columns;
     }
     public int getRows() {
         return 1;
