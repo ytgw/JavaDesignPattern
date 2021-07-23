24752da7f99ad2b1ae0a3e509dd305f5c7daa3ba
ytgw <tagawa0920@gmail.com>
2021-07-23 06:53:15 +0000

    ビジターパターンの練習問題2のコードを追加

---
+++ 13_visitor/q2/Directory.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Directory extends Entry {
+    private String name;
+    private ArrayList<Entry> dir = new ArrayList<Entry>();
+    public Directory(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getSize() {
+        SizeVisitor v = new SizeVisitor();
+        accept(v);
+        return v.getSize();
+    }
+    public Entry add(Entry entry) {
+        dir.add(entry);
+        return this;
+    }
+    public Iterator<Entry> iterator() {
+        return dir.iterator();
+    }
+    public void accept(Visitor v) {
+        v.visit(this);
+    }
+}
